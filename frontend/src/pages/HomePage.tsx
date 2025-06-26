30406050b62aa347af6e5c5f116cb5eab792ac91
Raviteja Kapalavayi <teja230@gmail.com>
2025-06-26 02:23:34 -0500

    Update button text from 'Start 3-Day Free Trial' to 'Connect Store'

--- frontend/src/pages/HomePage.tsx
+++ frontend/src/pages/HomePage.tsx
@@ -268,7 +268,7 @@ const HomePage = () => {
                   <svg className="w-6 h-6 mr-3" viewBox="0 0 24 24" fill="currentColor">
                     <path d="M12 2a10 10 0 100 20 10 10 0 000-20zm1 15h-2v-2h2v2zm0-4h-2V7h2v6z" />
                   </svg>
-                  Start 3-Day Free Trial
+                  Connect Store
                 </button>
               )
             )}
